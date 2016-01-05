70963e0ec0a0ca8afaacae894b6b6b2343f9533d
Thomas Krnc <Krncy@thomass-macbook-pro.local>
2016-01-05 15:50:01 +1100

    Initial Commit

---
+++ Movie Library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie Library
+//
+//  Created by Thomas Krnc on 5/01/2016.
+//  Copyright © 2016 Thomas Krnc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
